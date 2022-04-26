7aaa4a82f101dead0a880e1bebdd3350ad62d8c0
carlo-ddm <carloduminuco90@gmail.com>
2022-04-26 15:12:30 +0200

    impostazione htmal css struttura di base

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 5 secondi.
 // Dopo 5 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
 // **Consigli del giorno:**
 // * Pensate prima in italiano.
 // * Dividete in piccoli problemi la consegna.
