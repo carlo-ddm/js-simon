26d486ce79ccd00212d3720572b1bc4088ac5a36
carlo-ddm <carloduminuco90@gmail.com>
2022-04-27 02:00:17 +0200

    manca parte finale

--- script.js
+++ script.js
@@ -9,23 +9,50 @@
 // * Individuate gli elementi di cui avete bisogno per realizzare il programma.
 
 console.log('TEST');
-
+const container = document.querySelector('.container');
 const inizio = document.getElementById('start');
-const stampa = document.createElement('div');
-
 inizio.addEventListener('click',estrazione);
 
+const inputsArray = [];
 const gruppo = [];
+// F-ESTRAZIONE
 function estrazione() {
-  
   gruppo.length = 0;
-  for (let i = 1; i <= 5; i++) {
-    let nEstratto = Math.ceil(Math.random() * 10);
+  container.innerHTML = '';
+  for (let i = 0; i < 5; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    console.log(box);
+    container.append(box);
+    let nEstratto =  Math.ceil(Math.random() * 100);
     console.log('Singolo numero estratto -->',nEstratto);
     gruppo.push(nEstratto);
+    console.log(gruppo);
+    box.innerHTML = gruppo[i];
   }
-  
-  console.log(gruppo);
+  setTimeout(function(){
+    const selectEl = document.querySelectorAll('.box');
+    console.log('Array di elementi --> ',selectEl);
+    for (i = 0; i < selectEl.length; i++){
+      selectEl[i].innerHTML = '';
+    }
+    
+      setTimeout(function() {
+        for (i = 0; i < 5; i++){
+        const input = parseInt(prompt('Inserisci il ' + numero(i) + ' numero'));
+        inputsArray.push(input);
+        console.log('risposte: ',inputsArray);
+        }
+      }, 0)
+  }, 5000)
 }
 
-// Creare un ciclo for moltiplicare le estrazioni e pushare i risultati in un array
+function numero(indiceFor) {
+  switch(indiceFor) {
+    case 0: return 'primo';
+    case 1: return 'secondo';
+    case 2: return 'terzo';
+    case 3: return 'quarto';
+    case 4: return 'quinto';
+  }
+}
